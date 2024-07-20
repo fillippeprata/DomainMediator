ec477c6bbea07a430a37effb3155b7c973f65c70
Fillippe Prata <cid.fillippe@birdirx.com>
2024-07-20 14:10:46 -0300

    Claims array nullable -> JwtService.GenerateAccessToken method.

--- src/DomainMediator.WebApi/Jwt/JwtService.cs
+++ src/DomainMediator.WebApi/Jwt/JwtService.cs
@@ -7,22 +7,23 @@ namespace DomainMediator.WebApi.Jwt;
 
 public interface IJwtService
 {
-    JwtResponse GenerateAccessToken(Guid id, string userName, string[] userRoles);
+    JwtResponse GenerateAccessToken(Guid userId, string userName, string[] userRoles, Claim[]? claims = null);
 }
 
 internal class JwtServiceImp(IJwtConfiguration _jwtConfiguration) : IJwtService
 {
-    public JwtResponse GenerateAccessToken(Guid id, string userName, string[] userRoles)
+    public JwtResponse GenerateAccessToken(Guid userId, string userName, string[] userRoles, Claim[]? claims = null)
     {
         var claimsList = new List<Claim>
         {
-            new(JwtDefaultClaimKeys.userId.ToString(), id.ToString()),
+            new(JwtDefaultClaimKeys.userId.ToString(), userId.ToString()),
             new(ClaimTypes.Name, userName)
         };
         claimsList.AddRange(userRoles.Select(role => new Claim(ClaimTypes.Role, role)));
+        if(claims is not null) claimsList.AddRange(claims);
 
         var expiresIn = DateTime.UtcNow.AddMinutes(_jwtConfiguration.ExpirationInMinutes);
-        var key = Encoding.ASCII.GetBytes(_jwtConfiguration.Key);
+        var key = Encoding.ASCII.GetBytes(_jwtConfiguration.Key!);
 
         var tokenHandler = new JwtSecurityTokenHandler();
         var token = tokenHandler.CreateToken(new SecurityTokenDescriptor
@@ -39,7 +40,7 @@ internal class JwtServiceImp(IJwtConfiguration _jwtConfiguration) : IJwtService
         return new JwtResponse
         {
             UserName = userName,
-            UserId = id,
+            UserId = userId,
             AccessToken = stringToken,
             ExpirationDateTime = token.ValidTo,
             ExpiresInSeconds = Convert.ToInt64((token.ValidTo - DateTime.UtcNow).TotalSeconds)
